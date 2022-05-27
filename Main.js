7e39ea2b5e6b3adac6929369121d5776d607776f
Sebastian H <sebastian.harangus@jlg.ro>
2022-05-27 10:52:48 +0300

    Added utility functions

--- Main.js
+++ Main.js
@@ -14,11 +14,31 @@ for (var i=0; i < x.length; i++) {
         }
     }
 }
-console.log(pos);
+
+var start = new Position(0,0,0);
+var end = new Position(15000,15000,100);
+
+search(pos,start,end);
+
+function search(positions,start,end){
+    var openList=[];
+    var closedList=[];
+    if (isValid(start.x,start.y,start.z) == false || isValid(end.x,end.y,end.z) == false) {
+        print("Starting or End point is invalid");
+        return
+    }
+
+    if (isEnabled(start.enabled) == false || isEnabled(end.enabled) == false) {
+        print("Starting or End point is blocked");
+        return
+    }
+    openList.push(start);
+    console.log(openList);
+}
 
 function makeArr(startValue, stopValue, step){
     var arr =[];
-    for (var i= 0; i < (stopValue - startValue)/step; i++) {
+    for (var i= 0; i <= (stopValue - startValue)/step; i++) {
         arr.push(startValue + (step*i));
     }
     return arr;
@@ -28,4 +48,23 @@ function Position(x,y,z){
     this.x = x;
     this.y = y;
     this.z = z;
+    this.enabled = true; //is position available
+    this.wv = 0; // wind velocity
+    this.wd = 0; // wind direction
+}
+
+function isValid(x,y,z){
+    return (x >= 0) && (x <= 20000) && (y >= 0) && (y <= 20000) && (z >= 0) && (z <= 125)
+}
+
+function isEnabled(boolean){
+    return boolean
+}
+
+function isDestination(x,y,z){
+    return (x==end.x) && (y==end.y) && (z==end.z)
+}
+
+function calculateH (x,y,z){
+    return Math.sqrt(Math.pow((x-end.x),2)+Math.pow((y-end.y),2)+Math.pow((z-end.z),2));
 }
